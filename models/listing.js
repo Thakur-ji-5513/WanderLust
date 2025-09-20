245935f9d12fff532d8136492d8c64e6a220eae9
Thakur-ji-5513 <rudrakshthakur60@gmail.com>
2025-09-20 14:46:44 +0530

    image ur bug solved

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const reviews = require("./reviews");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -7,21 +8,25 @@ const listingSchema = new Schema({
     required: true,
   },
   description: String,
-  image: {
-    url: {
-      type: String,
-      default:
-        "https://plus.unsplash.com/premium_photo-1755882951321-34175bc2eda2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      set: (v) =>
-        v === ""
-          ? "https://plus.unsplash.com/premium_photo-1755882951321-34175bc2eda2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          : v,
-    },
-    filename: String,
-  },
+  // In models/listing.js
+image: {
+  type: String,
+  default:
+    "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  set: (v) =>
+    v === ""
+      ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+      : v,
+},
   price: Number,
   location: String,
   country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review"
+    }
+  ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
